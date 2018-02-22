2ea5d9aa12c5cedf15597b95455aa60a7072bbeb
Unknown <christsutamas@gmail.com>
2018-02-22 12:25:53 +0700

    สอบข้อที่ 1

--- exam1.cpp
+++ exam1.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
-
+using namespace std;
 int main() {
+    int a,b,cmd,reusult_1,reusult_2,reusult_3,reusult_4;
+    cout<<"Welcome to CLI Calculator V1.00"<<endl;
+    cout<<"Number 1 : ";
+    cin>>a;
+    cout<<"Number 2 : ";
+    cin>>b;
+    do{
+        cout<<"menu\n1.+\n2.-\n3.*\n4./\n5.quit"<<endl;
+        cin>>cmd;
+
+        if(cmd==1){
+            reusult_1=a+b;
+            cout<<"Result = "<<reusult_1<<endl;
+        }else if(cmd==2){
+            reusult_2=a-b;
+            cout<<"Result = "<<reusult_2<<endl;
+        }else if(cmd==3){
+            reusult_3=a*b;
+            cout<<"Result = "<<reusult_3<<endl;
+        }else if(cmd==4){
+            reusult_4=a/b;
+            cout<<"Result = "<<reusult_4<<endl;
+        }{break;}
+    }while(1);
+
+
 
         return 0;
 }
